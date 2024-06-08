e8a646bdb1eae734e09623aa960f3c42a1325c0b
Orangeorobosa123 <167538694+Orangeorobosa123@users.noreply.github.com>
2024-06-08 14:31:56 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,107 +1,141 @@
+# configured aws provider with proper credentials
 provider "aws" {
   region     = "us-east-1"
+  profile    = "Nora_keys"
 }
 
-# custom vpc
-resource "aws_vpc" "demo" {
+
+# Create a VPC
+
+resource "aws_vpc" "prodvpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
   enable_dns_hostnames = true
 
   tags = {
-    Name = "demo"
+    Name = "production_vpc"
   }
 }
-# create subnet
-resource "aws_subnet" "demo_subnet" {
-  vpc_id     = aws_vpc.demo.id
-  cidr_block = "10.0.1.0/24"
-  map_public_ip_on_launch = true
+
+# Create a Subnet
+
+resource "aws_subnet" "prodsubnet1" {
+  vpc_id            = aws_vpc.prodvpc.id
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
-
+  map_public_ip_on_launch = true
+  
   tags = {
-    Name = "demo_subnet"
+    Name = "prod-subnet"
   }
 }
 
-# create internet gateway
+
+#Create the Internet Gateway and attach it to the VPC
 resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.demo.id
+  vpc_id = aws_vpc.prodvpc.id
 
   tags = {
-    Name = "gw"
+    Name = "New"
   }
 }
 
-# create route table
-resource "aws_route_table" "example" {
-  vpc_id = aws_vpc.demo.id
+# Create a Route Table
+resource "aws_route_table" "prodroute" {
+  vpc_id = aws_vpc.prodvpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
   }
 
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.gw.id
+  }
+
   tags = {
-    Name = "demo_route_table"
+    Name = "RT"
   }
 }
 
-# route table association
+
+#Associate the subnet with the Route Table
 resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.demo_subnet.id
-  route_table_id = aws_route_table.example.id
+  subnet_id      = aws_subnet.prodsubnet1.id
+  route_table_id = aws_route_table.prodroute.id
 }
 
-# create security group
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic and all outbound traffic"
-  vpc_id      = aws_vpc.demo.id
+# Create a Security Group for the EC2 Instance
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web"
+  description = "Allow webserver inbound traffic"
+  vpc_id      = aws_vpc.prodvpc.id
+
+  ingress {
+    description = "Web Traffic from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    #cidr_blocks = ["${var.my_ip}/32"]
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+  ingress {
+    description = "HTTP"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1" # Any ip address/ any protocol
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
   tags = {
     Name = "allow_tls"
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = aws_vpc.demo.cidr_block
-  from_port         = 443
-  ip_protocol       = "tcp"
-  to_port           = 443
-}
-resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = aws_vpc.demo.cidr_block
-  from_port         = 80
-  ip_protocol       = "tcp"
-  to_port           = 80
-}
-resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = aws_vpc.demo.cidr_block
-  from_port         = 22
-  ip_protocol       = "tcp"
-  to_port           = 22
-}
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
-}
-resource "aws_instance" "foo" {
-  ami           = "ami-04b70fa74e45c3917" 
-  instance_type = "t2.micro"
-  subnet_id = aws_subnet.demo_subnet.id
-  vpc_security_group_ids = [aws_security_group.allow_tls.id]
-  availability_zone = "us-east-1a"
-  key_name = "Peterskey"
+
+# Create the EC2 instance and assign key pair
+resource "aws_instance" "firstinstance" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_web.id]
+  subnet_id              = aws_subnet.prodsubnet1.id
+  key_name               = "Peterkeys"
+  availability_zone      = "us-east-1a"
   count = 5
+  
+
 
   tags = {
-    Name = "server1"
+    Name = "Jenkins_Server"
   }
-
-
 }
+
+
